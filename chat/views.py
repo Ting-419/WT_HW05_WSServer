5e1fc0ba409836bc535ee919c69d7659c3f0d532
Aroxed <petrashenko@gmail.com>
2021-03-31 13:10:37 +0300

    outside message sending added

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,8 @@
+from asgiref.sync import async_to_sync
+from channels.layers import get_channel_layer
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import datetime
 
 def index(request):
     return render(request, 'chat/index.html')
@@ -9,3 +12,17 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
+
+
+def webhook(request):
+    channel_layer = get_channel_layer()
+    if not channel_layer.groups:
+        return HttpResponse('No any groups available')
+    async_to_sync(channel_layer.group_send)(
+        list(channel_layer.groups.keys())[0],
+        {
+            'type': 'chat_message',
+            'message': 'Hello from the Web. Current time is %s' % datetime.datetime.now()
+        }
+    )
+    return HttpResponse("Result is OK. Check Chat windows for a new message")
